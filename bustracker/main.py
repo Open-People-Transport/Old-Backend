dc3261053c808821efc302691728c82d9cc2cdf3
Dmitry Semenov <lonelyteapot@gmail.com>
2022-06-13 22:18:55 +0300

    Create City model and endpoint

--- bustracker/main.py
+++ bustracker/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from sqlmodel import Session, select
 
-from .database import create_db_tables
+from . import models
+from .database import create_db_tables, get_session
 
 app = FastAPI()
 
@@ -8,3 +10,19 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
     create_db_tables()
+
+
+@app.get("/", response_model=list[models.City])
+def read_cities(session: Session = Depends(get_session)):
+    cities = session.exec(select(models.City)).all()
+    return cities
+
+
+@app.post("/", response_model=models.City, status_code=status.HTTP_201_CREATED)
+def create_city(city: models.City, session: Session = Depends(get_session)):
+    if session.get(models.City, city.abbr):
+        raise HTTPException(status.HTTP_409_CONFLICT, detail="City already exists")
+    session.add(city)
+    session.commit()
+    session.refresh(city)
+    return city
--- bustracker/models.py
+++ bustracker/models.py
@@ -1,3 +1,12 @@
-from typing import Optional
+from typing import Any
 
 from sqlmodel import Field, SQLModel
+
+
+def example(value: Any) -> dict[str, Any]:
+    return {"example": value}
+
+
+class City(SQLModel, table=True):
+    abbr: str = Field(primary_key=True, max_length=16, schema_extra=example("es"))
+    name: str = Field(max_length=32, schema_extra=example("Example City"))
