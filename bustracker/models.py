c1fb16831d198cd6de5aa48ad8d6f69f0407ebf8
Dmitry Semenov <lonelyteapot@gmail.com>
2022-06-21 13:33:51 +0300

    Remove City model and endpoint

--- bustracker/models.py
+++ bustracker/models.py
@@ -7,8 +7,3 @@ from .database import BaseModel
 
 def example(value: Any) -> dict[str, Any]:
     return {"example": value}
-
-
-class City(BaseModel, table=True):
-    abbr: str = Field(primary_key=True, max_length=16, schema_extra=example("es"))
-    name: str = Field(max_length=32, schema_extra=example("Example City"))
