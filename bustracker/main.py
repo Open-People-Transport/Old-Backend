b5e5ae35f0fe5fe7224713587f1908b25d0db421
Dmitry Semenov <lonelyteapot@gmail.com>
2022-06-22 00:29:20 +0300

    Set up Strawberry

--- bustracker/main.py
+++ bustracker/main.py
@@ -1,10 +1,28 @@
-from fastapi import FastAPI
+import strawberry
+from fastapi import Depends, FastAPI
 from sqladmin import Admin
+from sqlmodel import Session
+from strawberry.fastapi import BaseContext, GraphQLRouter
 
 from .admin_models import NodeAdmin, RouteAdmin, StopAdmin, TypeAdmin  # type: ignore
-from .database import engine
+from .database import engine, get_session
+from .schemas import Query
+
+
+class Context(BaseContext):
+    def __init__(self, session: Session):
+        self.session = session
+
+
+async def get_context(session=Depends(get_session)):
+    return Context(session)
+
+
+schema = strawberry.Schema(Query)
+graphql_app = GraphQLRouter(schema, context_getter=get_context)
 
 app = FastAPI()
+app.include_router(graphql_app, prefix="/graphql")
 admin = Admin(app, engine)
 
 
