d76972b419e978109e71825a3501bce64b76eb85
Dmitry Semenov <lonelyteapot@gmail.com>
2022-06-26 01:50:50 +0300

    Refactor tests

--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -1,9 +1,50 @@
+import random
 from random import randrange
+from string import ascii_letters, digits
 from uuid import UUID
 
+import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 from uuid_extensions import uuid7
+
+from tests import conftest
+
+
+def _type():
+    return {
+        "name": "".join(random.choice(ascii_letters) for _ in range(12)),
+    }
+
+
+def _route(_type):
+    return {
+        "id": str(uuid7()),
+        "number": "".join(random.choice(digits) for _ in range(6)),
+        "type_name": _type["name"],
+    }
+
+
+def _node():
+    return {
+        "id": str(uuid7()),
+        "name": "".join(random.choice(ascii_letters) for _ in range(32)),
+    }
+
+
+@pytest.fixture
+def type():
+    return _type()
+
+
+@pytest.fixture
+def route(type):
+    return _route(type)
+
+
+@pytest.fixture
+def node():
+    return _node()
 
 
 def test_database_connection(session: Session):
@@ -14,131 +55,97 @@ def test_client_launch(client: TestClient):
     pass
 
 
-def test_type_create(client: TestClient):
-    response = client.put("/types/", json={"name": "bus"})
+def test_type_create(client: TestClient, type):
+    response = client.put("/types/", json=type)
     assert response.status_code == 200
-    assert response.json() == {"name": "bus"}
+    assert response.json() == type
 
 
-def test_type_read(client: TestClient):
-    response = client.put("/types/", json={"name": "bus"})
-    response = client.get("/types/bus")
+def test_type_read(client: TestClient, type):
+    response = client.put("/types/", json=type)
+    response = client.get("/types/" + type["name"])
     assert response.status_code == 200
-    assert response.json() == {"name": "bus"}
+    assert response.json() == type
 
 
-def test_type_read_all(client: TestClient):
-    response = client.put("/types/", json={"name": "bus"})
+def test_type_read_all(client: TestClient, type):
+    response = client.put("/types/", json=type)
     response = client.get("/types/")
     assert response.status_code == 200
-    assert response.json() == [{"name": "bus"}]
+    assert response.json() == [type]
 
 
-def test_type_update(client: TestClient):
-    response = client.put("/types/", json={"name": "bus"})
-    response = client.put("/types/bus", json={"name": "tram"})
+def test_type_update(client: TestClient, type):
+    response = client.put("/types/", json=type)
+    old_name = type["name"]
+    type = _type()
+    response = client.put("/types/" + old_name, json=type)
     assert response.status_code == 200
-    assert response.json() == {"name": "tram"}
+    assert response.json() == type
     response = client.get("/types/")
-    assert response.json() == [{"name": "tram"}]
+    assert response.json() == [type]
 
 
-def test_type_delete(client: TestClient):
-    response = client.put("/types/", json={"name": "bus"})
-    response = client.delete("/types/bus")
+def test_type_delete(client: TestClient, type):
+    response = client.put("/types/", json=type)
+    response = client.delete("/types/" + type["name"])
     assert response.status_code == 200
     assert response.json() == None
     response = client.get("/types/")
     assert response.json() == []
 
 
-def test_route_create_with_id(client: TestClient):
-    response = client.put("/types/", json={"name": "bus"})
-    id = str(uuid7())
-    data = {
-        "id": id,
-        "number": "1",
-        "type_name": "bus",
-    }
-    response = client.put("/routes/", json=data)
+def test_route_create_with_id(client: TestClient, type, route):
+    response = client.put("/types/", json=type)
+    response = client.put("/routes/", json=route)
     assert response.status_code == 200
-    assert response.json() == data
+    assert response.json() == route
 
 
-def test_route_create_without_id(client: TestClient):
-    response = client.put("/types/", json={"name": "bus"})
-    data = {
-        "number": "1",
-        "type_name": "bus",
-    }
-    response = client.put("/routes/", json=data)
+def test_route_create_without_id(client: TestClient, type, route):
+    del route["id"]
+    response = client.put("/types/", json=type)
+    response = client.put("/routes/", json=route)
     assert response.status_code == 200
-    id = UUID(response.json()["id"])
-    assert response.json() == ({"id": str(id)} | data)
+    route["id"] = str(UUID(response.json()["id"]))
+    assert response.json() == route
 
 
-def test_route_read(client: TestClient):
-    response = client.put("/types/", json={"name": "bus"})
-    id = str(uuid7())
-    data = {
-        "id": id,
-        "number": "1",
-        "type_name": "bus",
-    }
-    response = client.put("/routes/", json=data)
-    response = client.get(f"/routes/{id}")
+def test_route_read(client: TestClient, type, route):
+    response = client.put("/types/", json=type)
+    response = client.put("/routes/", json=route)
+    response = client.get("/routes/" + route["id"])
     assert response.status_code == 200
-    assert response.json() == data
+    assert response.json() == route
 
 
-def test_route_read_all(client: TestClient):
-    response = client.put("/types/", json={"name": "bus"})
-    id = str(uuid7())
-    data = {
-        "id": id,
-        "number": "1",
-        "type_name": "bus",
-    }
-    response = client.put("/routes/", json=data)
+def test_route_read_all(client: TestClient, type, route):
+    response = client.put("/types/", json=type)
+    response = client.put("/routes/", json=route)
     response = client.get("/routes/")
     assert response.status_code == 200
-    assert response.json() == [data]
+    assert response.json() == [route]
 
 
-def test_route_update(client: TestClient):
-    response = client.put("/types/", json={"name": "bus"})
-    response = client.put("/types/", json={"name": "tram"})
-    id = str(uuid7())
-    data = {
-        "id": id,
-        "number": "1",
-        "type_name": "bus",
-    }
-    response = client.put("/routes/", json=data)
-    data = {
-        "id": id,
-        "number": "2",
-        "type_name": "tram",
-    }
-    response = client.put("/routes/", json=data)
+def test_route_update(client: TestClient, type, route):
+    type2 = _type()
+    route2 = _route(type2)
+    route2["id"] = route["id"]
+    response = client.put("/types/", json=type)
+    response = client.put("/types/", json=type2)
+    response = client.put("/routes/", json=route)
+    response = client.put("/routes/", json=route2)
     assert response.status_code == 200
-    assert response.json() == data
+    assert response.json() == route2
     response = client.get("/routes/")
     assert response.status_code == 200
-    assert response.json() == [data]
+    assert response.json() == [route2]
 
 
-def test_route_delete(client: TestClient):
-    response = client.put("/types/", json={"name": "bus"})
-    response = client.put("/types/", json={"name": "tram"})
-    id = str(uuid7())
-    data = {
-        "id": id,
-        "number": "1",
-        "type_name": "bus",
-    }
-    response = client.put("/routes/", json=data)
-    response = client.delete(f"/routes/{id}")
+def test_route_delete(client: TestClient, type, route):
+    response = client.put("/types/", json=type)
+    response = client.put("/routes/", json=route)
+    response = client.delete(f"/routes/" + route["id"])
     assert response.status_code == 200
     assert response.json() == None
     response = client.get("/routes/")
@@ -146,78 +153,49 @@ def test_route_delete(client: TestClient):
     assert response.json() == []
 
 
-def test_node_create_with_id(client: TestClient):
-    id = str(uuid7())
-    data = {
-        "id": id,
-        "name": "Example St.",
-    }
-    response = client.put("/nodes/", json=data)
+def test_node_create_with_id(client: TestClient, node):
+    response = client.put("/nodes/", json=node)
     assert response.status_code == 200
-    assert response.json() == data
+    assert response.json() == node
 
 
-def test_node_create_without_id(client: TestClient):
-    data = {
-        "name": "Example St.",
-    }
-    response = client.put("/nodes/", json=data)
+def test_node_create_without_id(client: TestClient, node):
+    del node["id"]
+    response = client.put("/nodes/", json=node)
     assert response.status_code == 200
-    id = UUID(response.json()["id"])
-    assert response.json() == ({"id": str(id)} | data)
+    node["id"] = str(UUID(response.json()["id"]))
+    assert response.json() == node
 
 
-def test_node_read(client: TestClient):
-    id = str(uuid7())
-    data = {
-        "id": id,
-        "name": "Example St.",
-    }
-    response = client.put("/nodes/", json=data)
-    response = client.get(f"/nodes/{id}")
+def test_node_read(client: TestClient, node):
+    response = client.put("/nodes/", json=node)
+    response = client.get("/nodes/" + node["id"])
     assert response.status_code == 200
-    assert response.json() == data
+    assert response.json() == node
 
 
-def test_node_read_all(client: TestClient):
-    id = str(uuid7())
-    data = {
-        "id": id,
-        "name": "Example St.",
-    }
-    response = client.put("/nodes/", json=data)
+def test_node_read_all(client: TestClient, node):
+    response = client.put("/nodes/", json=node)
     response = client.get("/nodes/")
     assert response.status_code == 200
-    assert response.json() == [data]
+    assert response.json() == [node]
 
 
-def test_node_update(client: TestClient):
-    id = str(uuid7())
-    data = {
-        "id": id,
-        "name": "Example St.",
-    }
-    response = client.put("/nodes/", json=data)
-    data = {
-        "id": id,
-        "name": "Example Ave.",
-    }
-    response = client.put("/nodes/", json=data)
+def test_node_update(client: TestClient, node):
+    node2 = _node()
+    node2["id"] = node["id"]
+    response = client.put("/nodes/", json=node)
+    response = client.put("/nodes/", json=node2)
     assert response.status_code == 200
-    assert response.json() == data
+    assert response.json() == node2
     response = client.get("/nodes/")
     assert response.status_code == 200
-    assert response.json() == [data]
+    assert response.json() == [node2]
 
 
-def test_node_delete(client: TestClient):
-    id = str(uuid7())
-    data = {
-        "id": id,
-        "name": "Example Ave.",
-    }
-    response = client.put("/nodes/", json=data)
-    response = client.delete(f"/nodes/{id}")
+def test_node_delete(client: TestClient, node):
+    response = client.put("/nodes/", json=node)
+    response = client.delete(f"/nodes/" + node["id"])
     assert response.status_code == 200
     assert response.json() == None
     response = client.get("/nodes/")
