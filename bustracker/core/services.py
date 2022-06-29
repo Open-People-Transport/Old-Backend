cb619f795093c5d11589204e4e786b834fc57184
Dmitry Semenov <lonelyteapot@gmail.com>
2022-06-29 16:13:33 +0300

    Minor refactor

--- bustracker/core/services.py
+++ bustracker/core/services.py
@@ -12,19 +12,19 @@ from starlette.status import HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
 
 
 class Service:
-    def __init__(self, db: Session) -> None:
-        self.db = db
+    def __init__(self, session: Session) -> None:
+        self.session = session
 
 
 class TypeService(Service):
     def list(self) -> list[core.Type]:
         query = select(db.Type)
-        values = self.db.scalars(query).all()
+        values = self.session.scalars(query).all()
         result = list(map(core.Type.from_orm, values))
         return result
 
     def get(self, name: str) -> core.Type:
-        value = self.db.get(db.Type, name)
+        value = self.session.get(db.Type, name)
         if value is None:
             raise HTTPException(HTTP_404_NOT_FOUND)
         return core.Type.from_orm(value)
@@ -33,69 +33,69 @@ class TypeService(Service):
         if new in self:
             raise HTTPException(HTTP_409_CONFLICT)
         row = db.Type(name=new.name)
-        self.db.add(row)
-        self.db.commit()
+        self.session.add(row)
+        self.session.commit()
         # No refresh needed
         return core.Type.from_orm(row)
 
     def update(self, name: str, new: core.Type) -> core.Type:
-        row = self.db.get(db.Type, name)
+        row = self.session.get(db.Type, name)
         if row is None:
             raise HTTPException(HTTP_404_NOT_FOUND)
         row.name = new.name
         try:
-            self.db.commit()
+            self.session.commit()
         except IntegrityError as exc:
             raise HTTPException(HTTP_409_CONFLICT, str(exc.orig))
-        self.db.refresh(row)
+        self.session.refresh(row)
         return core.Type.from_orm(row)
 
     def delete(self, name: str) -> None:
-        row = self.db.get(db.Type, name)
+        row = self.session.get(db.Type, name)
         if row is None:
             raise HTTPException(HTTP_404_NOT_FOUND)
-        self.db.delete(row)
+        self.session.delete(row)
         try:
-            self.db.commit()
+            self.session.commit()
         except IntegrityError as exc:
             raise HTTPException(HTTP_409_CONFLICT, str(exc.orig))
 
     def __contains__(self, item: core.Type):
-        row = self.db.get(db.Type, item.name)
+        row = self.session.get(db.Type, item.name)
         return row is not None
 
 
 class RouteService(Service):
     def list(self) -> list[core.Route]:
         query = select(db.Route)
-        values = self.db.scalars(query).all()
+        values = self.session.scalars(query).all()
         result = list(map(core.Route.from_orm, values))
         return result
 
     def get(self, id: UUID) -> core.Route:
-        value = self.db.get(db.Route, id)
+        value = self.session.get(db.Route, id)
         if value is None:
             raise HTTPException(HTTP_404_NOT_FOUND)
         return core.Route.from_orm(value)
 
     def update(self, new: core.Route) -> core.Route:
-        row = self.db.get(db.Route, new.id)
+        row = self.session.get(db.Route, new.id)
         if row is None:
             row = db.Route(id=new.id)
-            self.db.add(row)
+            self.session.add(row)
         row.number = new.number
         row.type_name = new.type_name
-        self.db.commit()
-        self.db.refresh(row)
+        self.session.commit()
+        self.session.refresh(row)
         return core.Route.from_orm(row)
 
     def delete(self, id: UUID) -> None:
-        row = self.db.get(db.Route, id)
+        row = self.session.get(db.Route, id)
         if not row:
             raise HTTPException(HTTP_404_NOT_FOUND)
-        self.db.delete(row)
+        self.session.delete(row)
         try:
-            self.db.commit()
+            self.session.commit()
         except IntegrityError as exc:
             raise HTTPException(HTTP_409_CONFLICT, str(exc.orig))
 
@@ -103,33 +103,33 @@ class RouteService(Service):
 class NodeService(Service):
     def list(self) -> list[core.Node]:
         query = select(db.Node)
-        values = self.db.scalars(query).all()
+        values = self.session.scalars(query).all()
         result = list(map(core.Node.from_orm, values))
         return result
 
     def get(self, id: UUID) -> core.Node:
-        value = self.db.get(db.Node, id)
+        value = self.session.get(db.Node, id)
         if value is None:
             raise HTTPException(HTTP_404_NOT_FOUND)
         return core.Node.from_orm(value)
 
     def update(self, new: core.Node) -> core.Node:
-        row = self.db.get(db.Node, new.id)
+        row = self.session.get(db.Node, new.id)
         if row is None:
             row = db.Node(id=new.id)
-            self.db.add(row)
+            self.session.add(row)
         row.name = new.name
-        self.db.commit()
-        self.db.refresh(row)
+        self.session.commit()
+        self.session.refresh(row)
         return core.Node.from_orm(row)
 
     def delete(self, id: UUID) -> None:
-        row = self.db.get(db.Node, id)
+        row = self.session.get(db.Node, id)
         if not row:
             raise HTTPException(HTTP_404_NOT_FOUND)
-        self.db.delete(row)
+        self.session.delete(row)
         try:
-            self.db.commit()
+            self.session.commit()
         except IntegrityError as exc:
             raise HTTPException(HTTP_409_CONFLICT, str(exc.orig))
 
@@ -137,35 +137,35 @@ class NodeService(Service):
 class StopService(Service):
     def list(self) -> list[core.Stop]:
         query = select(db.Stop)
-        values = self.db.scalars(query).all()
+        values = self.session.scalars(query).all()
         result = list(map(self.model_to_schema, values))
         return result
 
     def get(self, id: UUID) -> core.Stop:
-        value = self.db.get(db.Stop, id)
+        value = self.session.get(db.Stop, id)
         if value is None:
             raise HTTPException(HTTP_404_NOT_FOUND)
         return self.model_to_schema(value)
 
     def update(self, new: core.Stop) -> core.Stop:
-        row = self.db.get(db.Stop, new.id)
+        row = self.session.get(db.Stop, new.id)
         if row is None:
             row = db.Stop(id=new.id)
-            self.db.add(row)
+            self.session.add(row)
         row.node_id = new.node_id
         shape = shapely.geometry.point.Point(new.lon, new.lat)
         row.location = geoalchemy2.shape.from_shape(shape)
-        self.db.commit()
-        self.db.refresh(row)
+        self.session.commit()
+        self.session.refresh(row)
         return self.model_to_schema(row)
 
     def delete(self, id: UUID) -> None:
-        row = self.db.get(db.Stop, id)
+        row = self.session.get(db.Stop, id)
         if not row:
             raise HTTPException(HTTP_404_NOT_FOUND)
-        self.db.delete(row)
+        self.session.delete(row)
         try:
-            self.db.commit()
+            self.session.commit()
         except IntegrityError as exc:
             raise HTTPException(HTTP_409_CONFLICT, str(exc.orig))
 
@@ -189,12 +189,12 @@ class RouteStopService(Service):
             query = query.where(db.RouteStop.route_id == route_id)
         if stop_id:
             query = query.where(db.RouteStop.stop_id == stop_id)
-        values = self.db.scalars(query).all()
+        values = self.session.scalars(query).all()
         result = list(map(core.RouteStop.from_orm, values))
         return result
 
     def get(self, route_id: UUID, stop_id: UUID) -> core.RouteStop:
-        value = self.db.get(db.RouteStop, (route_id, stop_id))
+        value = self.session.get(db.RouteStop, (route_id, stop_id))
         if value is None:
             raise HTTPException(HTTP_404_NOT_FOUND)
         return core.RouteStop.from_orm(value)
@@ -205,10 +205,10 @@ class RouteStopService(Service):
         stop_id: UUID,
         after_stop: UUID = None,
     ) -> core.RouteStop:
-        row = self.db.get(db.RouteStop, (stop_id, route_id))
+        row = self.session.get(db.RouteStop, (stop_id, route_id))
         if row is None:
             row = db.RouteStop(stop_id=stop_id, route_id=route_id)
-            self.db.add(row)
+            self.session.add(row)
 
         # TODO Proper distance calculation algorithm
         # Current solution:
@@ -218,35 +218,35 @@ class RouteStopService(Service):
         # * If not, insert 200 meters ahead of the previous (e.g. 200 -> 400).
         prev_stop: db.RouteStop | None
         if after_stop:
-            prev_stop = self.db.get(db.RouteStop, (route_id, after_stop))
+            prev_stop = self.session.get(db.RouteStop, (route_id, after_stop))
             if prev_stop is None:
                 raise HTTPException(HTTP_404_NOT_FOUND)
         else:
             query = select(db.RouteStop).order_by(desc(db.RouteStop.distance))
-            prev_stop = self.db.scalars(query).first()
+            prev_stop = self.session.scalars(query).first()
         prev_distance = prev_stop.distance if prev_stop else -200
         query = (
             select(db.RouteStop)
             .where(db.RouteStop.distance > prev_distance)
             .order_by(asc(db.RouteStop.distance))
         )
-        next_stop: db.RouteStop | None = self.db.scalars(query).first()
+        next_stop: db.RouteStop | None = self.session.scalars(query).first()
         if next_stop:
             current_distance = (prev_distance + next_stop.distance) // 2
         else:
             current_distance = prev_distance + 200
 
         row.distance = current_distance
-        self.db.commit()
-        self.db.refresh(row)
+        self.session.commit()
+        self.session.refresh(row)
         return core.RouteStop.from_orm(row)
 
     def delete(self, route_id: UUID, stop_id: UUID) -> None:
-        row = self.db.get(db.RouteStop, (route_id, stop_id))
+        row = self.session.get(db.RouteStop, (route_id, stop_id))
         if not row:
             raise HTTPException(HTTP_404_NOT_FOUND)
-        self.db.delete(row)
+        self.session.delete(row)
         try:
-            self.db.commit()
+            self.session.commit()
         except IntegrityError as exc:
             raise HTTPException(HTTP_409_CONFLICT, str(exc.orig))
